382762cd6108b74b672bd51f214e9323af38034c
Kiran-H-github <kiran_1994@hotmail.com>
2018-10-15 15:41:40 -0400

    Initial commit for camera calibtation cleanup

---
+++ intrinsic_and_extrinsic_optimise_real_data/Build/CMakeFiles/nlo_eg.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nlo_eg.dir/home/kiran/Desktop/camera_calibration_virtana_kiran/intrinsic_and_extrinsic_optimise_real_data/src/camera_calibration_real_data.cpp.o"
+  "nlo_eg.pdb"
+  "nlo_eg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nlo_eg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
